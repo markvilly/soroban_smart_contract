e34b13ef52ba73b1bb788c4673b6c4a714a881b9
Mark Paul <markpaulbusiness66@gmail.com>
2025-03-20 23:38:58 +0300

    refactoring the entire thing

--- contracts/hello-world/src/lib.rs
+++ contracts/hello-world/src/lib.rs
@@ -1,52 +1,16 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, symbol_short, vec, Env, String, Symbol, Vec};
-
-#[no_std]
-#[contract]
-pub struct Contract;
-
-// This is a sample contract. Replace this placeholder with your own contract logic.
-// A corresponding test example is available in `test.rs`.
-//
-// For comprehensive examples, visit <https://github.com/stellar/soroban-examples>.
-// The repository includes use cases for the Stellar ecosystem, such as data storage on
-// the blockchain, token swaps, liquidity pools, and more.
-//
-// Refer to the official documentation:
-// <https://developers.stellar.org/docs/build/smart-contracts/overview>.
-
-
-
-const LIBRARY_KEY: Symbol = symbol_short!("LIBRARY");
-
-pub struct Book{
-    pub title: Symbol,
-    pub author: Symbol,
-    pub year: u32
-}
-
-pub trait LibraryTrait {
-    fn initialize(env: Env);
-    fn add_book(env: Env, title: Symbol, author: Symbol, year: u32);
-    fn remove_book(env: Env, title: Symbol);
-    fn find_book(env: Env, title: Symbol)-> Option<Book>;
-    fn list_books(env: Env)-> Vec<Book>;
-
-}
-
-pub struct Library {
-    books: Vec<Book>
-}
-
+use soroban_sdk::{contract, contractimpl, symbol_short, Env, Symbol, Vec};
 
 #[contract]
 pub struct LibraryContract;
 
-#[contractimpl]
-impl Contract {
-    pub fn hello(env: Env, to: String) -> Vec<String> {
-        vec![&env, String::from_str(&env, "Hello"), to]
-    }
+const LIBRARY_KEY: Symbol = symbol_short!("LIBRARY");
+
+#[derive(Debug, Clone)]
+pub struct Book {
+    pub title: Symbol,
+    pub author: Symbol,
+    pub year: u32,
 }
 
 mod test;
--- contracts/hello-world/src/test.rs
+++ contracts/hello-world/src/test.rs
@@ -1,26 +1,15 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{vec, Env, String};
+use soroban_sdk::{vec, Env, String, symbol_short};
 
 #[test]
 fn test() {
-    let env = Env::default();
-    let contract_id = env.register(Contract, ());
-    let client = ContractClient::new(&env, &contract_id);
-
-    let words = client.hello(&String::from_str(&env, "Dev"));
-    assert_eq!(
-        words,
-        vec![
-            &env,
-            String::from_str(&env, "Hello"),
-            String::from_str(&env, "Dev"),
-        ]
-    );
+    
 }
 
 #[test]
-fn test2 () {
+fn test_initialize() {
 
 }
+
